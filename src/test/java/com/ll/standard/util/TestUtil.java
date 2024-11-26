6ee9e75903195f6f6fe317ab8f3e58dff91f453f
Tae-heeMin <hee66265@naver.com>
2024-11-26 14:09:27 +0900

    TestUtil

--- src/test/java/com/ll/standard/util/TestUtil.java
+++ src/test/java/com/ll/standard/util/TestUtil.java
@@ -1,10 +1,26 @@
 package com.ll.standard.util;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class TestUtil {
 
     public static Scanner getScanner(String input) {
         return new Scanner(input);
+    }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

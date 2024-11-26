8ecfe8b819bca14b1ac294ab239a06bea254986c
Tae-heeMin <hee66265@naver.com>
2024-11-26 15:10:57 +0900

    systemController

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,12 +1,16 @@
 package com.ll;
 
+import com.ll.domain.system.controller.systemController.SystemController;
+
 import java.util.Scanner;
 
 public class App {
     private final Scanner sc;
+    private final SystemController systemWiseSayingController;
 
     public App(Scanner sc) {
         this.sc = sc;
+        this.systemWiseSayingController = new SystemController();
     }
 
     public void run() {
@@ -17,7 +21,10 @@ public class App {
             System.out.println("명령) ");
             String cmd = sc.nextLine();
 
-            if ("종료".equals(cmd)) break;
+            if ("종료".equals(cmd)) {
+                systemWiseSayingController.actionExit();
+                break;
+            }
         }
     }
 

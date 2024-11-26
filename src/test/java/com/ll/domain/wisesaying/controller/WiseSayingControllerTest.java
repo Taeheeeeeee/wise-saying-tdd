61079e5a6471f58cf8376df1a9eeddbb91a3231a
Tae-heeMin <hee66265@naver.com>
2024-11-26 15:27:02 +0900

    3단계 테스트

--- src/test/java/com/ll/domain/wisesaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wisesaying/controller/WiseSayingControllerTest.java
@@ -8,8 +8,6 @@ import static org.assertj.core.api.Assertions.*;
 
 public class WiseSayingControllerTest {
 
-
-
     @Test
     @DisplayName("등록을 입력하면 작가와 내용을 입력 받는다.")
     void t4(){
@@ -22,5 +20,18 @@ public class WiseSayingControllerTest {
         assertThat(output)
                 .contains("명언: ")
                 .contains("작가: ");
+    }
+
+    @Test
+    @DisplayName("등록을 완료하면 명언번호가 출력된다.")
+    void t5(){
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(output)
+                .contains("1번 명령이 등록되었습니다.");
     }
 }

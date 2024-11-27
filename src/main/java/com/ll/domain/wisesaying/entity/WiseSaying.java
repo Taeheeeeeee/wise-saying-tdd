64f40a5dd34b0be45e85f012003f79d0dc66037c
Tae-heeMin <hee66265@naver.com>
2024-11-27 14:08:49 +0900

    lombok 추가

--- src/main/java/com/ll/domain/wisesaying/entity/WiseSaying.java
+++ src/main/java/com/ll/domain/wisesaying/entity/WiseSaying.java
@@ -1,41 +1,18 @@
 package com.ll.domain.wisesaying.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class WiseSaying {
     private int id;
     private String content;
     private String author;
 
-    public WiseSaying(int id, String content, String author) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
-
     public boolean isNew() {
         return id == 0;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
     }
 }

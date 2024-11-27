a73b575c4205075cf487c120791b214ed5c4540d
Tae-heeMin <hee66265@naver.com>
2024-11-27 12:23:50 +0900

    Util 테스트

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -1,38 +1,55 @@
 package com.ll.standard.util;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtilTest {
+    @BeforeAll
+    public static void beforeAll() {
+        Util.file.mkdir("temp");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        Util.file.rmdir("temp");
+    }
+
     @Test
     @DisplayName("파일을 생성할 수 있다.")
-    void t1(){
-        String filePath = "temp/text.txt";
+    public void t1() {
+        String filePath = "temp/test.txt";
 
         Util.file.touch(filePath);
+
         assertThat(
                 Util.file.exists(filePath)
         ).isTrue();
+
+        Util.file.delete(filePath);
     }
 
     @Test
-    @DisplayName("파일의 내용을 수정할 수 있고 읽을 수 있다.")
-    void t2(){
-        String filePath = "temp/text.txt";
+    @DisplayName("파일의 내용을 수정할 수 있고, 읽을 수 있다.")
+    public void t2() {
+        String filePath = "temp/test.txt";
 
         Util.file.set(filePath, "내용");
+
         assertThat(
                 Util.file.get(filePath, "")
         ).isEqualTo("내용");
-    }
 
+        Util.file.delete(filePath);
+    }
 
     @Test
     @DisplayName("파일을 삭제할 수 있다.")
-    void t3(){
-        String filePath = "temp/text.txt";
+    public void t3() {
+        String filePath = "temp/test.txt";
 
         Util.file.touch(filePath);
         Util.file.delete(filePath);
